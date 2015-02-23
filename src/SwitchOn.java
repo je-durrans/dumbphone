8fc8cd8bc07043f633e8e680e477e1980271ca91
je-durrans <je-durrans@users.noreply.github.com>
2015-02-23 21:12:38 +0000

    Update SwitchOn.java

--- src/SwitchOn.java
+++ src/SwitchOn.java
@@ -6,41 +6,58 @@ import java.io.*;
 
 
 public class SwitchOn extends JFrame implements MouseListener {
-    public void main(String[] args) {
-        //clearLines(lines);
-        /*boolean phoneOn = false; make this into if statement when putting into the main method*/
-        Date pressedTime;
-        long timeClicked;
-        String password;
-        String pass;
-        
-        @Override
-        public void mousePressed(MouseEvent e) {
-            pressedTime = System.getCurrentTimeMillis();
-        }    
-        public void mouseReleased(MouseEvent e) {         
-            timeClicked = new Date().getTime() - pressedTime.getTime();
-            if (timeClicked >= 2000) {
-                Ringtone.happySound();
-                System.out.println("The phone is now switched on");
-                phoneOn = true;
-                InputStreamReader in = new InputStreamReader(System.in);
-                BufferedReader br = new BufferedReader(in);
-                if (password=null) {
-                    System.out.println("Please set password: ");
-                    password = br.readLine();
-                    //go to main menu?
-                } else {
-                    while (!pass.equals(password)) {
-                        System.out.println("Please enter password: ");
-                        pass = br.readLine();
-                        if (pass.equals(password)){
-                            System.out.println("Well done, WELCOME");
-                            //go to main menu?
+
+    //clearLines(lines);
+    /*boolean phoneOn = false; make this into if statement when putting into the main method*/
+    long pressedTime;
+    long timeClicked;
+    String password;
+    String pass;
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        pressedTime = System.currentTimeMillis();
+    }
+    public void mouseReleased(MouseEvent e) {
+        timeClicked = new Date().getTime() - pressedTime;
+        if (timeClicked >= 2000) {
+            //Ringtone.happySound();
+            System.out.println("The phone is now switched on");
+            boolean phoneOn = true;
+            InputStreamReader in = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(in);
+            try {
+                    if (password == null) {
+                        System.out.println("Please set password: ");
+                        password = br.readLine();
+                        //go to main menu?
+                    } else {
+                        while (!pass.equals(password)) {
+                            System.out.println("Please enter password: ");
+                            pass = br.readLine();
+                            if (pass.equals(password)) {
+                                System.out.println("Well done, WELCOME");
+                                //go to main menu?
+                            }
                         }
-                }
-            }
+                    }
+            } catch(IOException ex) {}
         }
     }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+
+    }
 }
-}
+
