eb75eaf1a0f5b2db67db7cc0a699f5cd8f9fc6bf
Luke Simmons <LukeSimmons@users.noreply.github.com>
2015-02-23 19:17:37 +0000

    Has method for Ringtone, Happy Sound, Sad Sound and Tritone now

--- src/Ringtone.java
+++ src/Ringtone.java
@@ -1,17 +1,22 @@
-
-/* this is where we would put our ringtone IF WE HAD ONE **/
-
 import java.io.File;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.SourceDataLine;
-public class CallReceive {
-    final static String ringtone = "ringtone.wav";
+
+public class Ringtone {
+    
+    final static String ringtone = "Ringtone.wav";
+    final static String happySound = "HappySound.wav";
+    final static String sadSound = "SadSound.wav";
+    final static String tritone = "Tritone.wav";
     final static int buffer_size = 128000;
-    public static void main( String[] argv ) {
+    
+    public static void ringtone() {
+        
         try {
+            
             File audioFile = new File( ringtone );
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream( audioFile );
             AudioFormat audioFormat = audioInputStream.getFormat();
@@ -21,17 +26,126 @@ public class CallReceive {
             sourceLine.start();
             byte[] audioBuffer = new byte[ buffer_size ];
             int n = 0;
+            
             while ( n != -1 ) {
+                
                 n = audioInputStream.read( audioBuffer, 0, audioBuffer.length );
-                    if ( n >= 0 ) {
-                        sourceLine.write( audioBuffer, 0, n );
-                    }
+                
+                if ( n >= 0 ) {
+                    sourceLine.write( audioBuffer, 0, n );
                 }
-            sourceLine.drain();
-            sourceLine.close();
+            }
+            
+        sourceLine.drain();
+        sourceLine.close();
+        
         } catch ( Exception e ) {
+            
             e.printStackTrace();
             System.exit( 1 );
+            
+        }
+    }
+    
+    public static void happySound() {
+        
+        try {
+            
+            File audioFile = new File( happySound );
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream( audioFile );
+            AudioFormat audioFormat = audioInputStream.getFormat();
+            DataLine.Info info = new DataLine.Info( SourceDataLine.class, audioFormat );
+            SourceDataLine sourceLine = ( SourceDataLine ) AudioSystem.getLine( info );
+            sourceLine.open( audioFormat );
+            sourceLine.start();
+            byte[] audioBuffer = new byte[ buffer_size ];
+            int n = 0;
+            
+            while ( n != -1 ) {
+                
+                n = audioInputStream.read( audioBuffer, 0, audioBuffer.length );
+                
+                if ( n >= 0 ) {
+                    sourceLine.write( audioBuffer, 0, n );
+                }
+            }
+            
+        sourceLine.drain();
+        sourceLine.close();
+        
+        } catch ( Exception e ) {
+            
+            e.printStackTrace();
+            System.exit( 1 );
+            
+        }
+    }
+    
+    public static void sadSound() {
+        
+        try {
+            
+            File audioFile = new File( sadSound );
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream( audioFile );
+            AudioFormat audioFormat = audioInputStream.getFormat();
+            DataLine.Info info = new DataLine.Info( SourceDataLine.class, audioFormat );
+            SourceDataLine sourceLine = ( SourceDataLine ) AudioSystem.getLine( info );
+            sourceLine.open( audioFormat );
+            sourceLine.start();
+            byte[] audioBuffer = new byte[ buffer_size ];
+            int n = 0;
+            
+            while ( n != -1 ) {
+                
+                n = audioInputStream.read( audioBuffer, 0, audioBuffer.length );
+                
+                if ( n >= 0 ) {
+                    sourceLine.write( audioBuffer, 0, n );
+                }
+            }
+            
+        sourceLine.drain();
+        sourceLine.close();
+        
+        } catch ( Exception e ) {
+            
+            e.printStackTrace();
+            System.exit( 1 );
+            
+        }
+    }
+    
+    public static void tritone() {
+        
+        try {
+            
+            File audioFile = new File( tritone );
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream( audioFile );
+            AudioFormat audioFormat = audioInputStream.getFormat();
+            DataLine.Info info = new DataLine.Info( SourceDataLine.class, audioFormat );
+            SourceDataLine sourceLine = ( SourceDataLine ) AudioSystem.getLine( info );
+            sourceLine.open( audioFormat );
+            sourceLine.start();
+            byte[] audioBuffer = new byte[ buffer_size ];
+            int n = 0;
+            
+            while ( n != -1 ) {
+                
+                n = audioInputStream.read( audioBuffer, 0, audioBuffer.length );
+                
+                if ( n >= 0 ) {
+                    sourceLine.write( audioBuffer, 0, n );
+                }
+            }
+            
+        sourceLine.drain();
+        sourceLine.close();
+        
+        } catch ( Exception e ) {
+            
+            e.printStackTrace();
+            System.exit( 1 );
+            
         }
     }
 }
