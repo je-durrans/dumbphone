d914e392bd5025ca889aa5f4f6f76e6f2be8a45f
Luke Simmons <LukeSimmons@users.noreply.github.com>
2015-03-12 04:45:42 +0000

    Create Testing.java

---
+++ src/Testing.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * The test class Testing.
+ *
+ * @author  Luke Simmons
+ * @version 22/02/2015
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({PhonebookTesting.class, FunctionTesting.class})
+public class Testing {    
+}
