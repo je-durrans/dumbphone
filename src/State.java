ac347cb4d950e47ff5cfec785ac2decec6151d1e
je-durrans <je-durrans@users.noreply.github.com>
2015-03-12 10:58:40 +0000

    Update State.java

--- src/State.java
+++ src/State.java
@@ -1,5 +1,7 @@
 /**
  * Created by John on 23/02/2015.
+ * 
+ * Used to keep track of the current state of a dumbphone
  */
 public enum State {
     OFF, MAIN_MENU, CONTACTS_MENU, ADD_CONTACT, REMOVE_CONTACT,
