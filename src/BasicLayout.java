cb37793a533682bebd9bf2ad77251278003f3d5a
met8 <mtekman95@gmail.com>
2015-03-12 01:28:35 +0000

    Update BasicLayout.java

--- src/BasicLayout.java
+++ src/BasicLayout.java
@@ -7,57 +7,39 @@ import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Very basic layout of the interface of the 'dumb phone'. Provides all the elements required
- * for the interface, and a method to reset the text set on any of the TextField lines.
- * Alphanumeric keypad works in this version, which can be used to input characters to the terminal
- * when input from the user is required. Hash key serves as a toggle between the modes of the keypad,
- * the star button deletes the last character of the input and the zero button acts as the space bar,
- * when the input is in alphabetical state. 
- * When input is not required, the buttons are used to go through the menus, which is handled by two 
- * other classes; ButtonFunctions() and Display().
+ * Very basic layout of the interface of the 'dumb phone'. Provides all the elements required for the interface, and a method to reset the text set on any of the TextField lines.
+ * Alphanumeric keypad works in this version, which can be used to input characters to the terminal when input from the user is required. Hash key serves as a toggle between the modes 
+ * of the keypad, the star button deletes the last character of the input and the zero button acts as the space bar, when the input is in alphabetical state (either upper case or lower case). 
+ * When input is not required, the buttons are used to go through the menus, which is handled by two other classes; ButtonFunctions() and Display().
  * 
- * In this version, wherever the input is being made, it is stored in one variable, regardless of its type 
- * (wheterher its a contact name or a text message), and the user cannot choose between the fields when more 
- * than one field is offered for input. This should change in the next version and all the input required 
- * should be held in seperate variables.
+ * In this version, whenever more than one field of input is offered to the user, the general input variable acts as an intermediate variable and when the user is done with one field, the input
+ * is stored in new variables depending on which line it was made from.  
  *
- * 25/02/2015
+ * 11/03/2015
  *  
  * @author Mustafa Tekman
- * @version 1.2
+ * @version 1.3
  */
 public class BasicLayout {
 
     State state = State.OFF;  
     boolean passcodeSet = false;
     boolean target_is_name = true;
-    public static String password;
-
 
     
-    public static String input = ""; //a string to hold the input 
+    public static String input = ""; //a string to hold the input, when one field of input is offered to the user
+    public static String inputL1; //a string to hold the input from the first input field (when more than one field of input is offerred to the user)
+    public static String inputL2; //a string to hold the input from the second input field
     public static String inputType = "abc"; //initiate the input type to be lower case
     
-    //lastClick variables hold values relating to the time the buttons they correspond to were last clicked on. They are 
-    //instantiated at a randomly high value of 10000 so that the first if clause in the actionPerformed method in the event
-    //listeners of the buttons do not hold true and hence the counter gets instantiated at 0. 
-    public static long lastClick1 = 10000;
-    public static long lastClick2 = 10000;  
-    public static long lastClick3 = 10000;
-    public static long lastClick4 = 10000;
-    public static long lastClick5 = 10000;
-    public static long lastClick6 = 10000;
-    public static long lastClick7 = 10000;
-    public static long lastClick8 = 10000;
-    public static long lastClick9 = 10000;
-    public static long lastClick0 = 10000;
+    //lastClick variables hold values relating to the time the buttons they correspond to were last clicked on. They are instantiated at a randomly high value of 10000 so that the first if clause in the 
+    //actionPerformed method in the event listeners of the buttons do not hold true and hence the counter gets instantiated at 0. 
+    public static long lastClick1 = 10000; public static long lastClick2 = 10000; public static long lastClick3 = 10000; public static long lastClick4 = 10000; public static long lastClick5 = 10000; 
+    public static long lastClick6 = 10000; public static long lastClick7 = 10000; public static long lastClick8 = 10000; public static long lastClick9 = 10000; public static long lastClick0 = 10000;
    
     //declaring the lines
-    final static TextField line1 = new TextField();  
-    final static TextField line2 = new TextField(); 
-    final static TextField line3 = new TextField();  
-    final static TextField line4 = new TextField();  
-    final static TextField line5 = new TextField();  
+    final static TextField line1 = new TextField();  final static TextField line2 = new TextField(); final static TextField line3 = new TextField();  
+    final static TextField line4 = new TextField();  final static TextField line5 = new TextField();  
     
     final static ArrayList<TextField> lines = new ArrayList<TextField>(); //an array to store the lines
     
@@ -94,14 +76,8 @@ public class BasicLayout {
         //adding the event listeners to buttons
         ok_buttn.addActionListener( new ActionListener() {
             public void actionPerformed( ActionEvent ev ) {
-                //if(state==State.OFF){
-                    //do switchOn thing
-                    //long pressedTime = System.currentTimeMillis();
-                    //new SwitchOn(pressedTime);
-                //}else{
                     state = ButtonFunctions.pressOK(state);
                     display();
-                //}
             }
         } );
 
@@ -134,19 +110,16 @@ public class BasicLayout {
         } );
 
         buttn_2.addActionListener( new ActionListener() {
-            private AtomicInteger count2 = new AtomicInteger(0);
+            private AtomicInteger count2 = new AtomicInteger(0); //AtomicInteger is used when the buttons rely on the input() function, so that the function can work with a mutable variable and change the value of it as needed 
             private String[] values2 = { "A", "B", "C", "a" , "b", "c", "2"};
         
             public void actionPerformed( ActionEvent ev ) {
                 State newState = ButtonFunctions.press2(state);
                 if (newState == null) {
-                    
-                    
                     clearLines(lines);
                     lastClick1 = 10000; lastClick3 = 10000; lastClick4 = 10000; lastClick5 = 10000; lastClick6 = 10000; lastClick7 = 10000; lastClick8 = 10000; lastClick9 = 10000; lastClick0 = 10000;  
                     input( lastClick2, values2, count2 );
-                    lastClick2 = System.currentTimeMillis(); 
-         
+                    lastClick2 = System.currentTimeMillis();          
                 } else { state = newState; }
             
                 display();
@@ -156,14 +129,14 @@ public class BasicLayout {
         buttn_3.addActionListener( new ActionListener() {
             private AtomicInteger count3 = new AtomicInteger(0);
             private String[] values3 = { "D", "E", "F", "d" , "e", "f", "3"};
+            
             public void actionPerformed( ActionEvent ev ) { 
                 State newState = ButtonFunctions.press3(state);
                 if (newState == null) {
                     clearLines(lines);
                     lastClick1 = 10000; lastClick2 = 10000; lastClick4 = 10000; lastClick5 = 10000; lastClick6 = 10000; lastClick7 = 10000; lastClick8 = 10000; lastClick9 = 10000; lastClick0 = 10000; 
                     input(lastClick3, values3, count3);
-                    lastClick3 = System.currentTimeMillis();
-                
+                    lastClick3 = System.currentTimeMillis();               
                 } else { state = newState; }
                
                 display();
@@ -180,8 +153,7 @@ public class BasicLayout {
                     clearLines(lines);
                     lastClick1 = 10000; lastClick2 = 10000; lastClick3 = 10000; lastClick5 = 10000; lastClick6 = 10000; lastClick7 = 10000; lastClick8 = 10000; lastClick9 = 10000; lastClick0 = 10000;  
                     input(lastClick4, values4, count4);
-                    lastClick4 = System.currentTimeMillis();
-                
+                    lastClick4 = System.currentTimeMillis();                
                 } else { state = newState; }
                 
                 display();
@@ -191,6 +163,7 @@ public class BasicLayout {
         buttn_5.addActionListener( new ActionListener() {
             private AtomicInteger count5 = new AtomicInteger(0);
             private String[] values5 = { "J", "K", "L" , "j" , "k" , "l", "5" };
+            
             public void actionPerformed( ActionEvent ev ) {
                 boolean newState = ButtonFunctions.pressOther(state);
                 if (newState) { 
@@ -198,8 +171,7 @@ public class BasicLayout {
                     lastClick1 = 10000; lastClick2 = 10000; lastClick3 = 10000; lastClick4 = 10000; lastClick6 = 10000; lastClick7 = 10000; lastClick8 = 10000; lastClick9 = 10000; lastClick0 = 10000;  
                     input(lastClick5, values5, count5);
                     lastClick5 = System.currentTimeMillis();
-                    
-                } else { ButtonFunctions.pressOther( state ); }
+                                    } else { ButtonFunctions.pressOther( state ); }
                 
                 display();
             }
@@ -208,14 +180,14 @@ public class BasicLayout {
         buttn_6.addActionListener( new ActionListener() {
             private AtomicInteger count6 = new AtomicInteger(0);
             private String[] values6 = { "M", "N", "O", "m", "n", "o", "6"};
+            
             public void actionPerformed( ActionEvent ev ) {
                 boolean newState = ButtonFunctions.pressOther( state );
                 if (newState) { 
                     clearLines(lines);
                     lastClick1 = 10000; lastClick2 = 10000; lastClick3 = 10000; lastClick4 = 10000; lastClick5 = 10000; lastClick7 = 10000; lastClick8 = 10000; lastClick9 = 10000; lastClick0 = 10000;  
                     input(lastClick6, values6, count6);
-                    lastClick6 = System.currentTimeMillis();
-                    
+                    lastClick6 = System.currentTimeMillis();                    
                 } else { ButtonFunctions.pressOther( state ); }
 
                 display();
@@ -225,14 +197,14 @@ public class BasicLayout {
         buttn_7.addActionListener( new ActionListener() {
             private AtomicInteger count7 = new AtomicInteger(0);
             private String[] values7 = { "P", "Q", "R", "S", "p", "q", "r", "s", "7"};
+            
             public void actionPerformed( ActionEvent ev ) {
                 boolean newState = ButtonFunctions.pressOther( state );
                 if (newState) { 
                     clearLines(lines);
                     lastClick1 = 10000; lastClick2 = 10000; lastClick3 = 10000; lastClick4 = 10000; lastClick5 = 10000; lastClick6 = 10000; lastClick8 = 10000; lastClick9 = 10000; lastClick0 = 10000;  
                     input(lastClick7, values7, count7);
-                    lastClick7 = System.currentTimeMillis();
-                   
+                    lastClick7 = System.currentTimeMillis();                   
                  } else { ButtonFunctions.pressOther(state); }
                  
                  display();
@@ -259,14 +231,14 @@ public class BasicLayout {
         buttn_9.addActionListener( new ActionListener() {
             private AtomicInteger count9 = new AtomicInteger(0);
             private String[] values9 = { "W", "X", "Y", "Z", "w", "x", "y", "z", "9"};
+            
             public void actionPerformed( ActionEvent ev ) {
                 boolean newState = ButtonFunctions.pressOther( state );
                 if (newState) {
                     clearLines(lines);
                     lastClick1 = 10000; lastClick2 = 10000; lastClick3 = 10000; lastClick4 = 10000; lastClick5 = 10000; lastClick6 = 10000; lastClick7 = 10000; lastClick8 = 10000; lastClick0 = 10000; 
                     input(lastClick9, values9, count9);
-                    lastClick9 = System.currentTimeMillis();
-                    
+                    lastClick9 = System.currentTimeMillis();                    
                } else { ButtonFunctions.pressOther(state); }
                 
                display();
@@ -276,6 +248,7 @@ public class BasicLayout {
         buttn_0.addActionListener( new ActionListener() {
             private int count = 0;
             private String[] values0 = {" ","0"};
+            
             public void actionPerformed( ActionEvent ev ) {
                 boolean newState = ButtonFunctions.pressOther(state );
                 if (newState) {
@@ -296,12 +269,10 @@ public class BasicLayout {
                     if (inputType.equals("loweCase") || inputType.equals("ABC")) {
                         count = 0;
                     } else if (inputType.equals("123")) { count = 1; }
-             
-                    input += values0[count];
-        
-                }
-            
-                lastClick0 = System.currentTimeMillis();
+                        input += values0[count];
+                }            
+                    lastClick0 = System.currentTimeMillis();
+                    
                 } else { ButtonFunctions.pressOther( state ); }
               
                 display();
@@ -344,21 +315,16 @@ public class BasicLayout {
 
         //adding the lines and the buttons to the frame and setting the basic layout of them
         window.setLayout( null ); //no pre-set layout used in this version
-        line1.setBounds   ( 10, 10, 380, 40 ); window.add( line1 );
-        line2.setBounds   ( 10, 50, 380, 40 ); window.add( line2 );
-        line3.setBounds   ( 10, 90, 380, 40 ); window.add( line3 );
-        line4.setBounds   ( 10, 130, 380, 40 ); window.add( line4 );
-        line5.setBounds   ( 10, 170, 380, 40 ); window.add( line5 );
+        line1.setBounds ( 10, 10, 380, 40 ); window.add( line1 );   line2.setBounds ( 10, 50, 380, 40 ); window.add( line2 );   line3.setBounds( 10, 90, 380, 40 ); window.add( line3 );
+        line4.setBounds ( 10, 130, 380, 40 ); window.add( line4 );   line5.setBounds ( 10, 170, 380, 40 ); window.add( line5 );
 
-        ok_buttn.setBounds( 180, 215, 70, 40); window.add( ok_buttn );
-        buttn_1.setBounds ( 10,  260, 127, 30 ); window.add( buttn_1 );
-        buttn_2.setBounds ( 137, 260, 127, 30 ); window.add( buttn_2 );
-        buttn_3.setBounds ( 264, 260, 127, 30 ); window.add( buttn_3 );
+        ok_buttn.setBounds( 180, 215, 70, 40); window.add( ok_buttn );   buttn_1.setBounds ( 10,  260, 127, 30 ); window.add( buttn_1 );
+        buttn_2.setBounds ( 137, 260, 127, 30 ); window.add( buttn_2 );  buttn_3.setBounds ( 264, 260, 127, 30 ); window.add( buttn_3 );
 
         buttn_4.setBounds ( 10,  290, 127, 30 ); window.add( buttn_4 );
         buttn_5.setBounds ( 137, 290, 127, 30 ); window.add( buttn_5 );
         buttn_6.setBounds ( 264, 290, 127, 30 ); window.add( buttn_6 );
-
+  
         buttn_7.setBounds ( 10,  320, 127, 30 ); window.add( buttn_7 );
         buttn_8.setBounds ( 137, 320, 127, 30 ); window.add( buttn_8 );
         buttn_9.setBounds ( 264, 320, 127, 30 ); window.add( buttn_9 );
@@ -367,7 +333,6 @@ public class BasicLayout {
         star_buttn.setBounds ( 10,  350, 127, 30 ); window.add( star_buttn );
         sqar_buttn.setBounds ( 264, 350, 127, 30 ); window.add( sqar_buttn );
 
-
         window.setVisible(true);
         window.setLocationRelativeTo(null);
         window.setResizable(false);
@@ -375,7 +340,9 @@ public class BasicLayout {
 
     /**
      * Resets any text that has been set on any of the lines
+     * 
      * @param lines_array An array holding all the lines (TextFields) within the frame
+     * Mustafa
      */
     public static void clearLines( ArrayList<TextField> lines_array) {
         for ( TextField line : lines_array ) {
@@ -431,14 +398,23 @@ public class BasicLayout {
         }
     }
     
-    
+    /**
+     * Function used by buttons that hold alphanumeric characters when user is prompted for an input.
+     * Abstracts the input function so there's less repetition in the code
+     * 
+     * @param lastClick The corresponding long variable that holds the value of when the button was last pressed.
+     * @param values String array that holds all the alphanumeric values associated with the button.
+     * @param count A mutable integer that is used to set to refer to different values inside the values array (upper case, lower case and numeric values).
+     *              It is ncremented with each click and is used to refer to the values array to get the appropriate character. 
+     * 
+     * Mustafa
+     */
     public static void input( long lastClick, String[] values, AtomicInteger count ) {
-        
         long now = System.currentTimeMillis();
         int upperCaseBound = (values.length - 1) / 2;
         int lowerCaseBound = values.length - 1;
         
-        if (now - lastClick < 2000) {
+        if (now - lastClick < 2000) { //if the button is pressed again in two seconds, return the next character held by the key
             count.addAndGet(1);
             if (inputType.equals("ABC")) {
             if (count.get() >= upperCaseBound){
@@ -460,4 +436,26 @@ public class BasicLayout {
         lastClick = System.currentTimeMillis();
                    
              }
+    
+    /**
+     * Used when more than one field of input is available to the user and the user completes their input for the first field. When user presses the ok button to go to the next field, the temporary input variable's contents 
+     * is set to a new variable that is named to signify it holds the input from the first line of input. The temporary input variable is reset so that it's ready to hold the input that is to be made from the second line.
+     * 
+     * Mustafa
+     */
+    public static void line1InputDone() {
+        inputL1 = input;
+        input = "";       
+    }
+    
+    /**
+     * Similar to line1InputDone(), this method is used when more  than one field of input is available to the user and the user completes their input to the second field. The contents of the temporary input variable is 
+     * copied to a new variable that is named so that it's obvious it holds the input from the second line of input. The general input variable is reset so that it can hold new values from other fields as user continues on to other menus.
+     * 
+     * Mustafa 
+     */
+    public static void line2InputDone() {
+        inputL2 = input;
+        input = "";
+    }
 }
