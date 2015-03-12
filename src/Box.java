c4832524e8b6e1d16159046de18eb4f13939a938
je-durrans <je-durrans@users.noreply.github.com>
2015-03-12 10:51:08 +0000

    Update Box.java

--- src/Box.java
+++ src/Box.java
@@ -1,5 +1,8 @@
 /**
  * Created by John on 11/02/2015.
+ * 
+ * Container for a boolean variable which must
+ * be in a static field for access from an inner class.
  */
 public class Box {
     public boolean b;
