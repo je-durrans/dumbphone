2c74131460f58cda0e5a275314342dae90c57843
jm588 <jm588@users.noreply.github.com>
2015-03-12 10:44:58 +0000

    Update TextSend.java

--- src/TextSend.java
+++ src/TextSend.java
@@ -10,30 +10,26 @@ import java.net.Socket;
 * A class sending input text up to 64 characters between server and client 
 * 
 * @author Kate Moksina 
-* @version 1.0
+* @version 2.0
 */
 
 public class TextSend {
   final static String HOST  =  "127.0.0.1"; /* localhost */
   final static int    PORT  =  8888;
 
-  public static void main( String[] argv ) throws IOException {
+  public TextSend(String[] text) throws IOException{
     Socket s = new Socket( HOST, PORT );
-    
-    InputStream  in   =  s.getInputStream();
     OutputStream out  =  s.getOutputStream();
-
-    BufferedReader reader = new BufferedReader( new InputStreamReader( in ) );
     PrintWriter writer = new PrintWriter( out );
     String content="";
-    for(int i = 0; i<argv.length;i++){
+    for(int i = 0; i<text.length;i++){
         while(content.length()<64){
-        content=content+argv[i]+" ";
+        content=content+text[i]+" ";
     }
     }
     writer.print( content+"\r\n" );
     writer.flush();
-
     s.close();
+   
   }
 }
