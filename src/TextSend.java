97d59c03eda40ef71ea6fdd8c6b759738172aab5
jm588 <jm588@users.noreply.github.com>
2015-03-12 11:05:57 +0000

    Update TextSend.java

--- src/TextSend.java
+++ src/TextSend.java
@@ -16,17 +16,20 @@ import java.net.Socket;
 public class TextSend {
   final static String HOST  =  "127.0.0.1"; /* localhost */
   final static int    PORT  =  8888;
-
+  static String content="";
   public TextSend(String[] text) throws IOException{
     Socket s = new Socket( HOST, PORT );
     OutputStream out  =  s.getOutputStream();
     PrintWriter writer = new PrintWriter( out );
-    String content="";
+    
     for(int i = 0; i<text.length;i++){
         while(content.length()<64){
         content=content+text[i]+" ";
     }
     }
+    /*
+     * following print statement is used for testing. Actual output is used in ButtonFunctions
+     */
     writer.print( content+"\r\n" );
     writer.flush();
     s.close();
